580568537299682b197a0e1c0469d80d96a0d1f0
Bonnerup123 <56426813+Bonnerup123@users.noreply.github.com>
2020-11-24 14:17:23 +0100

    Trajectory added

--- Source/CrustCrawlerKinematics.h
+++ Source/CrustCrawlerKinematics.h
@@ -3,20 +3,29 @@
 class CrustCrawlerKinematics
 {
 public:
-	double ShoulderDistanceFromBase = 500;
-	double ShoulderHeightFromBase = 0;
+	double ShoulderDistanceFromBase = 400;
+	double ShoulderHeightFromBase = 100;
 	struct Angles {
 		double theta1;
 		double theta2;
 		double theta3;
 		double theta4;
 	};
-	struct Pos {
-		double x, y, z;
+	struct CurrentAngles {
+		double theta0_1;
+		double theta0_2;
+		double theta0_3;
+		double theta0_4;
+	};
+	struct CurrentVelocity {
+		double dtheta1;
+		double dtheta2;
+		double dtheta3;
+		double dtheta4;
 	};
 	arma::Mat<double> TargetMatrix = arma::zeros(4, 4);
-	Pos  ForwardKinematics(double t1, double t2, double t3, double t4);
-	Angles InverseKinematics(double x, double y, double z);
+	arma::Mat<double>  ForwardKinematics(double t1, double t2, double t3, double t4);
+	Angles InverseKinematics(arma::Mat<double> TargetMatrix);
 	
 protected:
 	double PI = arma::datum::pi;
